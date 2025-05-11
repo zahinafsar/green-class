1ca7de2795839d5219aa97b2e3a04840aea6a333
zahinafsar <afsarzahin@gmail.com>
2025-05-11 14:41:09 +0600

    add login e2e test

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Script
+        {/* <Script
           id="pushalert"
           dangerouslySetInnerHTML={{
             __html: `
@@ -42,7 +42,7 @@ export default function RootLayout({
               }(document, "script"));
             `,
           }}
-        />
+        /> */}
         <TanstackQueryProvider>
           {children}
           <Toaster />
---
+++ tests/login.spec.ts
@@ -0,0 +1,49 @@
+import { test, expect } from "@playwright/test";
+
+test("Check if the ui is visible", async ({ page }) => {
+  await page.goto("http://localhost:4200/login");
+
+  const wb = page.getByText("Welcome Back");
+  const eyc = page.getByText("Enter your credentials to");
+  const emailInput = page.getByRole("textbox", { name: "name@example.com" });
+  const passwordInput = page.getByRole("textbox", { name: "••••••••" });
+  const signinButton = page.getByRole("button", { name: "Sign in" });
+
+  await expect(wb).toBeVisible();
+  await expect(eyc).toBeVisible();
+  await expect(emailInput).toBeVisible();
+  await expect(passwordInput).toBeVisible();
+  await expect(signinButton).toBeVisible();
+});
+
+test("Login", async ({ page }) => {
+  await page.goto("http://localhost:4200/login");
+
+  const emailInput = page.getByRole("textbox", { name: "name@example.com" });
+  const passwordInput = page.getByRole("textbox", { name: "••••••••" });
+  const signinButton = page.getByRole("button", { name: "Sign in" });
+
+  await emailInput.fill("afsarzahin@gmail.com");
+  await passwordInput.fill("zahin@123");
+  await signinButton.click();
+
+  const chatText = page.getByText("Chats");
+
+  await expect(chatText).toBeVisible({ timeout: 10000 });
+});
+
+test("Wrong password", async ({ page }) => {
+  await page.goto("http://localhost:4200/login");
+
+  const emailInput = page.getByRole("textbox", { name: "name@example.com" });
+  const passwordInput = page.getByRole("textbox", { name: "••••••••" });
+  const signinButton = page.getByRole("button", { name: "Sign in" });
+
+  await emailInput.fill("afsarzahin@gmail.com");
+  await passwordInput.fill("zahin@1234");
+  await signinButton.click();
+
+  const chatText = page.getByText("Invalid credentials");
+
+  await expect(chatText).toBeVisible({ timeout: 30000 });
+});
